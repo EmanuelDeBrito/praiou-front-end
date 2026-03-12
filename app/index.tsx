97b94c2edd0073f1843a05e3dbe65cb8abb607eb
Emanuel de Brito <emanuelbrit16@gmail.com>
2026-03-11 22:33:29 -0300

    Creating Project and Configuring Expo Router

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const Screen = () => {
+    return(
+        <View>
+            <Text>Ola mundo</Text>
+        </View>
+    )
+}
+
+export default Screen
