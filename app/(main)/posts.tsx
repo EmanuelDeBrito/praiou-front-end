ae00b7d45335a65bc62c7671e9360882859a056c
Emanuel de Brito <emanuelbrit16@gmail.com>
2026-04-08 09:38:44 -0300

    Organizing Layout Default Style

--- app/(main)/posts.tsx
+++ app/(main)/posts.tsx
@@ -1,12 +1,26 @@
-import { Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 import { SafeContainer } from "../../components/general/safe-container"
 
 const Screen = () => {
     return(
         <SafeContainer>
-            <Text>Posts</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>Posts</Text>
+            </View>
         </SafeContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: '600'
+    }
+})
 
 export default Screen
