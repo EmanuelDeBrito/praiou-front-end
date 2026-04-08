aa8821ce65122effeb5d9b71dadff64a17f14360
Emanuel de Brito <emanuelbrit16@gmail.com>
2026-04-07 21:22:47 -0300

    Creating Verify Token Function

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,18 @@
 import { req } from "../libs/req"
 
+export const verifyToken = async (token: string) => {
+    const request = await req.get(
+        "/token",
+        {
+            headers: {
+                'Authorization': token
+            }
+        }
+    )
+
+    return request.data
+}
+
 export const register = async (nome: string, email: string, senha: string) => {
     const request = await req.post(
         "/registrar",
